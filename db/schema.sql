96b33e8e75c902318c88a2c96a1f1b32b3871529
Luis Ramirez <luis21x@outlook.com>
2021-11-16 21:32:19 -0500

    starter files and schema file created

---
+++ db/schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT DEFAULT null,
+FOREIGN KEY (role_id) REFERENCES roles(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
+ON DELETE SET NULL
+);
+
+
+
+
+
