dec1fedd11ea6ceb15f770516525b0d0c7fe22e7
Luis Ramirez <luis21x@outlook.com>
2021-11-18 23:13:02 -0500

    renames server js to querys, all view options are prompted to user and respond accordingly. still working on adding roles

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+const querys = require('./querys');
+
+const initPrompt = [
+    {
+    type: "list",
+    message:"*** Employee Tracker ***",
+    name: "action",
+    choices: [
+        'View all departments', 
+        'View all roles', 
+        'View all employees',
+        'add a department',
+        'add a role',
+        'add an employee',
+        'update an employee',
+        ],
+    },
+];
+
+inquirer.prompt(initPrompt).then((response) => {
+    //based on user response call upon the queries
+    switch (response.action){
+        case "View all departments":
+            querys.getDepartments();
+            break;
+        case "View all roles":
+            querys.getRoles();
+            break;
+        case "View all employees":
+            querys.getEmployee();
+            break;
+    }
+});
--- querys.js
+++ querys.js
@@ -24,51 +24,85 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the movies_db database.`)
   );
-
+module.exports = {
     //view all departments
+getDepartments:function getDepartments() {   
   db.query(`SELECT * FROM DEPARTMENT`, (err, result) => {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.table(result);
   });
+},
 
-  //view all positions with departments
+
+  //view all roles w departments
+getRoles: function getRoles(){ 
   db.query(`SELECT roles.title AS Job_Title,roles.id AS Role_Id, department.name AS department, roles.salary AS Salary FROM roles JOIN department ON roles.department_id = department.id;`, (err, result) => {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.table(result);
   });
+},  
 
-  //view all roles 
+  //view all employees
+getEmployee: function getEmployees() {
   db.query(`SELECT e1.id, e1.first_name, e1.last_name, roles.title, department.name AS department, roles.salary, CONCAT(e2.first_name,' ',e2.last_name) AS manager FROM employee AS e1 JOIN roles ON e1.role_id = roles.id JOIN department ON roles.department_id = department.id LEFT JOIN employee AS e2 ON e1.manager_id= e2.id;`, (err, result) => {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.table(result);
   });
+},
 
   //insert new department
+addDepartment: function addDepartment() {
   db.query(`INSERT INTO department (name) VALUES (?);`,'User-input2' ,(err, result) => {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.table(result);
   });
+},
+};
 
-  //insert new role
-  db.query(`INSERT INTO roles (name) VALUES (?,?,?);`,'new role',11000.00,4 ,(err, result) => {
+
+  //insert new role, create drop down for department, and a select to get all departments
+function addRole(){
+  db.query(`INSERT INTO roles (title,salary, department_id) VALUES (?,?,?);`, ['new role', 11000.00, 4] ,(err, result) => {
     if (err) {
       console.log(err);
     }
     console.log(result);
   });
+};
+  //insert employee, select all roles and all employees 
+function addEmployee() {
+  db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);`, ['John', 'Doe', 2, Luis] ,(err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+};
+
+  //update employee. select all employees, grab role_id, update title to dropdown role;
+function updateEmployeeRole(){
+  db.query(`UPDATE roles SET title = "?" WHERE id = ?`, ['John', 1] ,(err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+};
+
+
+
 
   //port listen
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+
 
  // SELECT e1.id, e1.first_name, e1.last_name, roles.title, department.name AS department, roles.salary, CONCAT(e2.first_name,' ',e2.last_name) AS manager FROM employee AS e1 JOIN roles ON e1.role_id = roles.id JOIN department ON roles.department_id = department.id LEFT JOIN employee AS e2 ON e1.manager_id= e2.id;
   
+ 
